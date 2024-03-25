cf3fb0e5456ab4fac9e87be4cd3c12b5b8c58b25
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-25 19:41:11 +0800

    2 коммит через терминал

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-console.log('Выражения в определениях\n');
-let who = "dragon's" + ' mother';
-console.log(who);
+console.log('   Выражения в определениях\n');
+let who = "dragon's" + ' mother\n';
+console.log(who); 
+
+let rublesPerDollar = 60;
+let dollarsCount = 50 * 1.25; // 62.5
+let rublesCount = dollarsCount * rublesPerDollar; // 3750
+
+console.log('The price is ' + rublesCount + ' rubles\n');
+// => The price is 3750 rubles 
+
+console.log('   Переменные и конкатенация\n');
+//склеивание 2 строки
+let what = 'Kings' + 'road\n';
+console.log(what); // => Kingsroad 
+//конкатенирование строки и одной переменной
+let first = 'Kings';
+let what1 = first + 'road\n';
+console.log(what1); // => Kingsroad
